bbd95772ccaf54ab023d7e56b1a3140df11e1e9a
Scott Rock <scottrockbiz@gmail.com>
2018-09-16 15:14:40 -0700

    Angular registration page with validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'registration-app';
+  user = new User();
+  registered = false;
+
+  onSubmit() {
+    console.log('user submitted', this.user);
+    this.registered = true;
+  }
 }
---
+++ src/app/user.ts
@@ -0,0 +1,12 @@
+export class User {
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  password: string = '';
+  passwordConfirm: string = '';
+  address: string = '';
+  unit: string = '';
+  city: string = '';
+  state: string = '';
+  lucky: string = '';
+}
